7bd269790e46f7a96f1310522dd7ae7879a7a16d
yijiahe <yi.jiahe00@gmail.com>
2022-07-21 13:56:27 +0000

    Create workers with running threads in ThreadPool

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
-pub struct ThreadPool;
+use std::thread;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
+    }
+}
+
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -11,7 +29,13 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool  {
         assert!(size > 0);
 
-        ThreadPool
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
